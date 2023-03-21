cd7a0f705dbef0b0c339b576381eb73151e0a113
jefferson <jefferson.avelar@catolica.edu.sv>
2023-03-21 12:04:38 -0600

    ptactica2

--- webApipractica/Models/equiposContext.cs
+++ webApipractica/Models/equiposContext.cs
@@ -12,6 +12,27 @@ namespace webApipractica.Models
         }
         public DbSet<equipos> equipos { get; set; }
 
+        public DbSet<carreras> carreras { get; set; }
+
+        public DbSet<estados_equipo> estados_equipos { get; set; }
+
+        public DbSet<estados_reserva> estados_reserva { get; set; }
+
+        public DbSet<facultades> facultades { get; set; }
+
+        public DbSet<marcas> marcas { get; set; }
+        public DbSet<reservas> reservas { get; set; }
+
+        public DbSet<tipo_equipo> tipo_equipos { get; set; }
+
+        public DbSet<usuarios> usuarios { get; set; }
+
+
+
+
+
+
+
 
     }
 }
